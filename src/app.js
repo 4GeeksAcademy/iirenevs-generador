9476fcb0ef2fbad8bd7a88c176b3bbd49c61e0a7
Irene VS <147383929+iireneVS@users.noreply.github.com>
2023-12-08 22:27:04 +0000

    modificado

--- src/app.js
+++ src/app.js
@@ -9,3 +9,34 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+document.querySelector("#btn").addEventListener("click", () => {
+  document.querySelector("#excuse").innerHTML = generarExcusa();
+});
+function generarExcusa() {
+  let who = ["El perrito", "El dragon", "El unicornio", "Un ave fénix"];
+  let action = ["se comió", "mojó", "rompió", "quemó"];
+  let what = ["mis deberes", "la comida", "la casa"];
+  let when = [
+    "después de salir.",
+    "cuando llegamos.",
+    "mientras no estaba.",
+    "cuando salimos a pasear.",
+    "mientras cantaba."
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
